76cf3f6ca779e111fd7cad872d188a71828d2472
Samarthya Singh <samarthya.singh@sprinklr.com>
2024-01-30 12:37:54 +0530

    Added some queries and mutations

--- server/db_functions.js
+++ server/db_functions.js
@@ -1,27 +1,58 @@
 const fs = require("fs");
 
 function getAllUsers() {
-    try {
-        const res = fs.readFileSync("db.json", 'utf-8');
-        return JSON.parse(res);
-    } catch (err) {
-        console.log(err);
-        throw err;
-    }
+    const res = fs.readFileSync("db.json", 'utf-8');
+    return JSON.parse(res);
 }
 
 function addUser(user) {
     const allUsers = getAllUsers();
 
-    allUsers.push(user);
+    for(let existing_user of allUsers) {
+        if(existing_user.username === user.username) throw new Error("Duplicate username!");
+    }
 
-    fs.writeFile("server/db.json", JSON.stringify(allUsers, null, 4), (err) => {
-        if (err) { console.error(err); return; };
-        console.log("File has been created");
-    });
+    allUsers.push(user);
+    fs.writeFileSync("db.json", JSON.stringify(allUsers, null, 4));
+}
+
+
+function getUserByUsername(username) {
+    const allUsers = getAllUsers();
+    return allUsers.find(user => user.username === username);
+}
+
+function updateUser(username, new_user) {
+    const allUsers = getAllUsers();
+    
+    let index = -1;
+    for(let i = 0; i < allUsers.length; i++) {
+        if(allUsers[i].username === username) {
+            index = i;
+            break;
+        }
+    }
+
+    if(index === -1) throw new Error("No such user!");
+
+    allUsers[index] = {
+        ...allUsers[index], ...new_user
+    }
+
+    fs.writeFileSync("db.json", JSON.stringify(allUsers, null, 4));
+    return allUsers[index];
+}
+
+function getLeaderboard() {
+    const allUsers = getAllUsers();
+    allUsers.sort((a, b) => b.max_score - a.max_score);
+    return allUsers;
 }
 
 module.exports = {
     getAllUsers,
-    addUser
+    addUser,
+    getUserByUsername,
+    updateUser,
+    getLeaderboard
 }
--- server/index.js
+++ server/index.js
@@ -1,13 +1,20 @@
-var express = require("express")
-var { graphqlHTTP } = require("express-graphql")
-var { buildSchema } = require("graphql")
-const { getAllUsers } = require("./db_functions.js")
+const express = require("express")
+const { graphqlHTTP } = require("express-graphql")
+const { buildSchema } = require("graphql")
+const { getAllUsers, addUser, getUserByUsername, updateUser, getLeaderboard } = require("./db_functions.js")
+const { v4: uuidv4 } = require('uuid');
 
 // Construct a schema, using GraphQL schema language
-var schema = buildSchema(`
+const schema = buildSchema(`
   type Query {
-    hello: String
-    users: [User]
+    all_users: [User]
+    user(username: String!): User
+    leaderboard: [User]
+  }
+
+  type Mutation {
+    create_user(username: String!, password: String!): User
+    update_max_score(username: String!, new_max_score: Int!): User
   }
 
   type User {
@@ -19,16 +26,31 @@ var schema = buildSchema(`
 `)
 
 // The root provides a resolver function for each API endpoint
-var root = {
-    hello: () => {
-        return "Hello world!"
-    },
-    users: () => {
+const root = {
+    all_users: () => {
         return getAllUsers();
+    },
+    user: ({ username }) => {
+        return getUserByUsername(username);
+    },
+    leaderboard: () => {
+        return getLeaderboard();
+    },
+    create_user: ({ username, password }) => {
+        try {
+            const newUser = { username, password, max_score: 0, id: uuidv4() };
+            addUser(newUser);
+            return newUser;
+        } catch(err) {
+            throw err;
+        }
+    },
+    update_max_score: ({ username, new_max_score }) => {
+        return updateUser(username, { max_score: new_max_score });
     }
 }
 
-var app = express()
+const app = express()
 app.use(
     "/graphql",
     graphqlHTTP({
@@ -37,5 +59,6 @@ app.use(
         graphiql: true,
     })
 )
+
 app.listen(4000)
 console.log("Running a GraphQL API server at http://localhost:4000/graphql")
